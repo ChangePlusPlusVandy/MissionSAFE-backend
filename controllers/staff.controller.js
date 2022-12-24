deffcb035a8377801e99405c1bb60b973474e55f
alexbawa <alexbawa@gmail.com>
2022-12-24 14:03:56 -0600

    Refactor staff controller, populate youth controller

--- controllers/staff.controller.js
+++ controllers/staff.controller.js
@@ -1,8 +1,28 @@
 const Staff = require("../models/Staff").model;
 
-const updateActiveStaff = async (fireID, update) => {
+const getAllStaff = async () => {
     try {
-        const staff = await Staff.findOneAndUpdate({fireID: fireID}, {active: update});
+        const allStaff = await Staff.find({});
+        if(!allStaff) throw new Error("No staff found");
+        return allStaff;
+    } catch (err) {
+        throw err;
+    }
+}
+
+const getStaffByID = async (fireID) => {
+    try {
+        const staff = await Staff.findOne({fireID: fireID});
+        if(!staff) throw new Error("Staff not found");
+        return staff;
+    } catch (err) {
+        throw err;
+    }
+}
+
+const updateStaff = async (fireID, update) => {
+    try {
+        const staff = await Staff.findOneAndUpdate({fireID: fireID}, update);
         if(!staff) throw new Error("Staff not found");
     } catch (err) {
         throw err;
@@ -10,5 +30,7 @@ const updateActiveStaff = async (fireID, update) => {
 }
 
 module.exports = { 
-    updateActiveStaff,
+    getAllStaff,
+    getStaffByID,
+    updateStaff,
 }
--- routers/youth.router.js
+++ routers/youth.router.js
@@ -1,7 +1,50 @@
 const express = require("express");
+const { getAllYouth, getYouthByID, updateYouth } = require("../controllers/youth.controller");
 
 const youthRouter = express.Router();
 youthRouter.use(express.json());
+
+// GET all Youth documents
+youthRouter.get('/', async(_req, res) => {
+    try {
+        let allYouth = await getAllYouth();
+        res.status(200).send(allYouth);
+    } catch (err) {
+        res.status(404).send(err);
+    }
+})  
+
+// GET Youth document matching param fireID
+youthRouter.get('/:fireID', async(req, res) => {
+    try {
+        let youth = await getYouthByID(req.params.fireID);
+        res.status(200).send(youth);
+    } catch (err) {
+        res.status(404).send(err);
+    }
+})
+
+// PUT active to true for Youth document matching 
+//     param fireID
+youthRouter.put('/:fireID/activate', async(req, res) => {
+    try {
+        await updateYouth(req.params.fireID, {active: true});
+        res.status(200).send("Youth activated");
+    } catch (err) {
+        res.status(500).send(err);
+    }
+})
+
+// PUT active to false for Youth document matching 
+//     param fireID
+youthRouter.put('/:fireID/deactivate', async(req, res) => {
+    try {
+        await updateYouth(req.params.fireID, {active: false});
+        res.status(200).send("Youth deactivated");
+    } catch (err) {
+        res.status(500).send(err);
+    }
+})
 
 module.exports = {
     youthRouter
