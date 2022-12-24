7f7ec985daf8576a414bd34ad109fe1a9cb079d7
Jianger <ethantianshujiang@gmail.com>
2022-12-23 18:54:36 -0700

    use findOneAndUpdate

--- controllers/staff.controller.js
+++ controllers/staff.controller.js
@@ -6,10 +6,21 @@ const staffExample = (req, res) => {
 
 const updateActiveStaff = async (fireID, update) => {
     try {
-        const staff = await Staff.findOne({fireID: fireID});
+        const staff = await Staff.findOneAndUpdate({fireID: fireID}, {active: false});
     if(!staff) throw new Error("Staff not found");
 
-    staff.active = update;
+    await staff.save();
+    } catch (err) {
+        throw err;
+    }
+}
+
+// TODO: pass in fire ID, boolean to set user's active property to true
+const activateStaff = async (fireID) => {
+    try {
+        const staff = await Staff.findOneAndUpdate({fireID: fireID}, {active: true});
+    if(!staff) throw new Error("Staff not found");
+
     await staff.save();
     } catch (err) {
         throw err;
