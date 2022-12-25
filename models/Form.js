fc45d55628ec6dbbaeb3c71dbcbf64606650d858
alexbawa <alexbawa@gmail.com>
2022-12-25 15:53:43 -0600

    Build out form model

--- models/Form.js
+++ models/Form.js
@@ -2,8 +2,12 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 let Form = new Schema({
-    date: {type: Date},
-})
+    date: { type: Date, required: true },
+    content: { type: String, required: true },
+    programs: { type: [String], required: true },
+    associated_youth_id: { type: String, default: null},
+    associated_event_id: { type: String, default: null},
+});
 
 module.exports = {
     model: mongoose.models.Form || mongoose.model("Form", Form),
