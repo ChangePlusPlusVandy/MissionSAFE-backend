e4866d0badeab0247fd9028fc886418541985d96
salomondush <63796975+salomondush@users.noreply.github.com>
2023-04-17 04:53:59 -0500

    search form endpoints

--- controllers/form.controller.js
+++ controllers/form.controller.js
@@ -3,57 +3,82 @@ const Event = require("../models/Event").model;
 const Form = require("../models/Form").model;
 
 const addFormToYouth = async (fireID, options) => {
-    let youth = await Youth.findOne({fireID});
-    if(!youth) throw new Error("Youth not found");
+	let youth = await Youth.findOne({ fireID });
+	if (!youth) throw new Error("Youth not found");
 
-    const newNote = new Form({
-        date: new Date(),
-        content: options.content,
-        programs: options.programs,
-        associated_youth_id: fireID,
-    })
-    await newNote.save();
+	const newNote = new Form({
+		date: new Date(),
+		content: options.content,
+		programs: options.programs,
+		associated_youth_id: fireID,
+	});
+	await newNote.save();
 
-    youth.attached_forms.push(newNote._id);
-    await youth.save();
-}
+	youth.attached_forms.push(newNote._id);
+	await youth.save();
+};
 
 const addFormToEvent = async (eventCode, options) => {
-    let event = await Event.findOne({code: eventCode});
-    if(!event) throw new Error("Event not found");
+	let event = await Event.findOne({ code: eventCode });
+	if (!event) throw new Error("Event not found");
 
-    const newNote = new Form({
-        name: options.name,
-        description: options.description,
-        date: new Date(),
-        content: options.content,
-        programs: options.programs,
-        associated_event_id: eventCode,
-    })
-    await newNote.save();
+	const newNote = new Form({
+		name: options.name,
+		description: options.description,
+		date: new Date(),
+		content: options.content,
+		programs: options.programs,
+		associated_event_id: eventCode,
+	});
+	await newNote.save();
 
-    event.attached_forms.push(newNote._id);
-    await event.save();
-}
+	event.attached_forms.push(newNote._id);
+	await event.save();
+};
 
 const getFormByID = async (id) => {
-    const form = await Form.findById(id);
+	const form = await Form.findById(id);
 
-    if(!form) throw new Error("Form not found");
-    return {
-        _id: form._id,
-        name: form.name,
-        description: form.description,
-        date: form.date,
-        content: form.content,
-        programs: form.programs,
-        associated_youth: await Youth.findById(form.associated_youth_id),
-        associated_event: await Event.findById(form.associated_event_id)
-    }
-}
+	if (!form) throw new Error("Form not found");
+	return {
+		_id: form._id,
+		name: form.name,
+		description: form.description,
+		date: form.date,
+		content: form.content,
+		programs: form.programs,
+		associated_youth: await Youth.findById(form.associated_youth_id),
+		associated_event: await Event.findById(form.associated_event_id),
+	};
+};
+
+const getFormsByEventCode = async (eventCode) => {
+	const event = await Event.findOne({ code: eventCode });
+
+	console.log("event", event);
+
+	// get a list of all the form ids associated with the event
+	const forms = event.attached_forms.map(async (formID) => {
+		// Batch request for forms
+		return await Form.findOne({ _id: formID });
+	});
+
+	console.log("forms", forms);
+
+	if (!forms) throw new Error("Forms not found");
+	return Promise.all(forms);
+};
+
+const getAllForms = async () => {
+	const forms = await Form.find();
+	if (!forms) throw new Error("Forms not found");
+	return forms;
+};
 
 module.exports = {
-    addFormToYouth,
-    addFormToEvent,
-    getFormByID
-}
+	addFormToYouth,
+	addFormToEvent,
+	getFormByID,
+	getFormsByEventCode,
+	getAllForms,
+};
--- routers/form.router.js
+++ routers/form.router.js
@@ -1,23 +1,41 @@
 const express = require("express");
-const { 
-    getFormByID 
-} = require("../controllers/form.controller");
+const { getFormByID, getFormsByEventCode, getAllForms } = require("../controllers/form.controller");
 
 const formRouter = express.Router();
 formRouter.use(express.json());
 
 // GET a Form by its mongodb id
-formRouter.get('/:id', async(req, res) => {
+formRouter.get("/:id", async (req, res) => {
+	try {
+		let form = await getFormByID(req.params.id);
+		res.status(200).send(form);
+	} catch (err) {
+		console.log(err);
+		res.status(500).send(err);
+	}
+});
 
-    try {
-        let form = await getFormByID(req.params.id);
-        res.status(200).send(form);
-    } catch (err) {
-        console.log(err);
-        res.status(500).send(err);
-    }
-})
+// GET all Forms by event code
+formRouter.get("/byEvent/:eventCode", async (req, res) => {
+	try {
+		let forms = await getFormsByEventCode(req.params.eventCode);
+		res.status(200).send(forms);
+	} catch (err) {
+		console.log(err);
+		res.status(500).send(err);
+	}
+});
+
+// GET all Forms
+formRouter.get("/", async (_req, res) => {
+	try {
+		let forms = await getAllForms();
+		res.status(200).send(forms);
+	} catch (err) {
+		console.log(err);
+	}
+});
 
 module.exports = {
-    formRouter
+	formRouter,
 };
