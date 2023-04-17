2076eec379dd670b2463ae962955ba74fed1d573
salomondush <63796975+salomondush@users.noreply.github.com>
2023-04-17 03:11:45 -0500

    fix get youth by all parameters

--- controllers/youth.controller.js
+++ controllers/youth.controller.js
@@ -3,91 +3,99 @@ const Event = require("../models/Event").model;
 const Form = require("../models/Form").model;
 
 const getAllYouth = async () => {
-    const allYouth = await Youth.find({});
-    if(!allYouth) throw new Error("No youth found");
-    return allYouth;
-}
+	const allYouth = await Youth.find({});
+	if (!allYouth) throw new Error("No youth found");
+	return allYouth;
+};
 
 const createYouth = async (options) => {
-    const newYouth = new Youth(options)
-    await newYouth.save();
-    return newYouth;
-}
+	const newYouth = new Youth(options);
+	await newYouth.save();
+	return newYouth;
+};
 
 const getYouthByActive = async (active) => {
-    const youth = await Youth.find({active});
-    if(!youth) throw new Error("Youth not found");
-    return youth;
-}
+	const youth = await Youth.find({ active });
+	if (!youth) throw new Error("Youth not found");
+	return youth;
+};
 
 const getYouthByID = async (fireID) => {
-    const youth = await Youth.findOne({fireID});
-    if(!youth) throw new Error("Youth not found");
-    return youth;
-}
+	const youths = await Youth.find({ fireID });
+	if (!youths) throw new Error("Youth not found");
+	return youths;
+};
 
 const getYouthByEmail = async (email) => {
-    const youth = await Youth.findOne({email});
-    if(!youth) throw new Error("Youth not found");
-    return youth;
-}
+	// get all Youth documents with an email field that contains the param email
+	const youths = await Youth.find({ email: { $regex: email, $options: "i" } });
+
+	// const youth = await Youth.findOne({email});
+	if (!youths) throw new Error("Youth not found");
+	return youths;
+};
 
 const getYouthByProgram = async (program) => {
-    const youth = await Youth.find({programs: program});
-    if(!youth) throw new Error("Youth not found");
-    return youth;
-}
+	// get all youth documents if the programs array field contains the param program
+	const youths = await Youth.find({ programs: { $regex: program, $options: "i" } });
+
+	// const youth = await Youth.find({ programs: program });
+	if (!youths) throw new Error("Youth not found");
+	return youths;
+};
 
 const getFormsForYouth = async (fireID) => {
-    const youth = await Youth.findOne({fireID});
-    if(!youth) throw new Error("Youth not found");
+	const youth = await Youth.findOne({ fireID });
+	if (!youth) throw new Error("Youth not found");
 
-    let forms = youth.attached_forms.map(async formID => { // Batch request for forms
-        return Form.findById(formID);
-    })
+	let forms = youth.attached_forms.map(async (formID) => {
+		// Batch request for forms
+		return Form.findById(formID);
+	});
 
-    return await Promise.all(forms); // Wait for all requests
-}
+	return await Promise.all(forms); // Wait for all requests
+};
 
 const getEventsForYouth = async (fireID) => {
-    const youth = await Youth.findOne({fireID});
-    if(!youth) throw new Error("Youth not found");
+	const youth = await Youth.findOne({ fireID });
+	if (!youth) throw new Error("Youth not found");
 
-    let events = youth.attended_events.map(async eventCode => { // Batch request for events
-        return Event.findOne({code: eventCode});
-    })
+	let events = youth.attended_events.map(async (eventCode) => {
+		// Batch request for events
+		return Event.findOne({ code: eventCode });
+	});
 
-    return await Promise.all(events); // Wait for all requests
-}
+	return await Promise.all(events); // Wait for all requests
+};
 
 const updateYouth = async (fireID, update) => {
-    const youth = await Youth.findOneAndUpdate({fireID}, update);
-    if(!youth) throw new Error("Youth not found");
-}
+	const youth = await Youth.findOneAndUpdate({ fireID }, update);
+	if (!youth) throw new Error("Youth not found");
+};
 
 const attendEvent = async (email, eventCode) => {
-    let event = await Event.findOne({code: eventCode});
-    if(!event) throw new Error("Event not found");
+	let event = await Event.findOne({ code: eventCode });
+	if (!event) throw new Error("Event not found");
 
-    let youth = await Youth.findOne({email});
-    if(!youth) throw new Error("Youth not found");
+	let youth = await Youth.findOne({ email });
+	if (!youth) throw new Error("Youth not found");
 
-    event.attended_youth.push(youth.fireID);
-    youth.attended_events.push(eventCode);
+	event.attended_youth.push(youth.fireID);
+	youth.attended_events.push(eventCode);
 
-    await event.save();
-    await youth.save();
-}
+	await event.save();
+	await youth.save();
+};
 
-module.exports = { 
-    getAllYouth,
-    createYouth,
-    getYouthByActive,
-    getYouthByID,
-    getYouthByEmail,
-    getYouthByProgram,
-    getFormsForYouth,
-    getEventsForYouth,
-    updateYouth,
-    attendEvent,
-}
+module.exports = {
+	getAllYouth,
+	createYouth,
+	getYouthByActive,
+	getYouthByID,
+	getYouthByEmail,
+	getYouthByProgram,
+	getFormsForYouth,
+	getEventsForYouth,
+	updateYouth,
+	attendEvent,
+};
--- routers/youth.router.js
+++ routers/youth.router.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const {
-  getAllYouth,
-  createYouth,
-  getYouthByActive,
-  getYouthByID,
-  getYouthByEmail,
-  getYouthByProgram,
-  getFormsForYouth,
-  getEventsForYouth,
-  updateYouth,
+	getAllYouth,
+	createYouth,
+	getYouthByActive,
+	getYouthByID,
+	getYouthByEmail,
+	getYouthByProgram,
+	getFormsForYouth,
+	getEventsForYouth,
+	updateYouth,
 } = require("../controllers/youth.controller");
 const { addFormToYouth } = require("../controllers/form.controller");
 
@@ -17,127 +17,127 @@ youthRouter.use(express.json());
 
 // GET all Youth documents
 youthRouter.get("/", async (_req, res) => {
-  try {
-    let allYouth = await getAllYouth();
-    res.status(200).send(allYouth);
-  } catch (err) {
-    res.status(404).send(err);
-  }
+	try {
+		let allYouth = await getAllYouth();
+		res.status(200).send(allYouth);
+	} catch (err) {
+		res.status(404).send(err);
+	}
 });
 
 // POST adding new Youth document
 youthRouter.post("/", async (req, res) => {
-  try {
-    let newYouth = await createYouth(req.body);
-    res.status(201).send(newYouth);
-  } catch (err) {
-    res.status(500).send(err);
-  }
+	try {
+		let newYouth = await createYouth(req.body);
+		res.status(201).send(newYouth);
+	} catch (err) {
+		res.status(500).send(err);
+	}
 });
 
 // GET active Youth
 youthRouter.get("/active", async (_req, res) => {
-  try {
-    let activeYouth = await getYouthByActive(true);
-    res.status(200).send(activeYouth);
-  } catch (err) {
-    res.status(404).send(err);
-  }
+	try {
+		let activeYouth = await getYouthByActive(true);
+		res.status(200).send(activeYouth);
+	} catch (err) {
+		res.status(404).send(err);
+	}
 });
 
 // GET inactive Youth
 youthRouter.get("/inactive", async (_req, res) => {
-  try {
-    let inactiveYouth = await getYouthByActive(false);
-    res.status(200).send(inactiveYouth);
-  } catch (err) {
-    res.status(404).send(err);
-  }
+	try {
+		let inactiveYouth = await getYouthByActive(false);
+		res.status(200).send(inactiveYouth);
+	} catch (err) {
+		res.status(404).send(err);
+	}
 });
 
 // GET Youth document matching param fireID
 youthRouter.get("/byID/:fireID", async (req, res) => {
-  try {
-    let youth = await getYouthByID(req.params.fireID);
-    res.status(200).send(youth);
-  } catch (err) {
-    res.status(404).send(err);
-  }
+	try {
+		let youths = await getYouthByID(req.params.fireID);
+		res.status(200).send(youths);
+	} catch (err) {
+		res.status(404).send(err);
+	}
 });
 
 // GET Youth document matching param email
 youthRouter.get("/byEmail/:email", async (req, res) => {
-  try {
-    let youth = await getYouthByEmail(req.params.email);
-    res.status(200).send(youth);
-  } catch (err) {
-    res.status(404).send(err);
-  }
+	try {
+		let youths = await getYouthByEmail(req.params.email);
+		res.status(200).send(youths);
+	} catch (err) {
+		res.status(404).send(err);
+	}
 });
 
 // GET Youth documents enrolled in param program
 youthRouter.get("/byProgram/:program", async (req, res) => {
-  try {
-    let youth = await getYouthByProgram(req.params.program);
-    res.status(200).send(youth);
-  } catch (err) {
-    res.status(404).send(err);
-  }
+	try {
+		let youths = await getYouthByProgram(req.params.program);
+		res.status(200).send(youths);
+	} catch (err) {
+		res.status(404).send(err);
+	}
 });
 
 // PUT active to true for Youth document matching
 //     param fireID
 youthRouter.put("/activate/:fireID", async (req, res) => {
-  try {
-    await updateYouth(req.params.fireID, { active: true });
-    res.status(200).send("Youth activated");
-  } catch (err) {
-    res.status(500).send(err);
-  }
+	try {
+		await updateYouth(req.params.fireID, { active: true });
+		res.status(200).send("Youth activated");
+	} catch (err) {
+		res.status(500).send(err);
+	}
 });
 
 // PUT active to false for Youth document matching
 //     param fireID
 youthRouter.put("/deactivate/:fireID", async (req, res) => {
-  try {
-    await updateYouth(req.params.fireID, { active: false });
-    res.status(200).send("Youth deactivated");
-  } catch (err) {
-    res.status(500).send(err);
-  }
+	try {
+		await updateYouth(req.params.fireID, { active: false });
+		res.status(200).send("Youth deactivated");
+	} catch (err) {
+		res.status(500).send(err);
+	}
 });
 
 // PUT new Form in notes for Youth matching param
 //     fireID
 youthRouter.put("/form/:fireID", async (req, res) => {
-  try {
-    await addFormToYouth(req.params.fireID, req.body);
-    res.status(200).send("Form added");
-  } catch (err) {
-    res.status(500).send(err);
-  }
+	try {
+		await addFormToYouth(req.params.fireID, req.body);
+		res.status(200).send("Form added");
+	} catch (err) {
+		res.status(500).send(err);
+	}
 });
 
 // GET all Forms for youth matching param fireID
 youthRouter.get("/forms/:fireID", async (req, res) => {
-  try {
-    let forms = await getFormsForYouth(req.params.fireID);
-    res.status(200).send(forms);
-  } catch (err) {
-    res.status(500).send(err);
-  }
+	try {
+		let forms = await getFormsForYouth(req.params.fireID);
+		res.status(200).send(forms);
+	} catch (err) {
+		res.status(500).send(err);
+	}
 });
 
 // GET all Events for youth matching param fireID
 youthRouter.get("/events/:fireID", async (req, res) => {
-  try {
-    let events = await getEventsForYouth(req.params.fireID);
-    res.status(200).send(events);
-  } catch (err) {
-    res.status(500).send(err);
-  }
+	try {
+		let events = await getEventsForYouth(req.params.fireID);
+		res.status(200).send(events);
+	} catch (err) {
+		res.status(500).send(err);
+	}
 });
 
 module.exports = {
-  youthRouter,
+	youthRouter,
 };
